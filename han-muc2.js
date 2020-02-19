77d80f7bdfac77101c8992fd5fb90e00389d93c4
ANH NGUYEN DUC <ndanh@fresher.local>
2020-02-20 04:02:27 +0700

    Update han-muc2.html

---
+++ han-muc2.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    
+
+
+    $("#datepicker1").datepicker({
+        uiLibrary: 'bootstrap4'
+
+    });
+    $('#datepicker2').datepicker({
+        uiLibrary: 'bootstrap4'
+
+    });
+});
+// JavaScript source code
